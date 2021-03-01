d0d23c58ee5478288c6cbc99d1c6337530bfdb50
ankitlimbad <ankitlimbad369@gmail.com>
2021-03-01 18:29:24 +0530

    add swiper demo

--- App.js
+++ App.js
@@ -9,13 +9,15 @@ import AppStateDemo from './AppState';
 import EasingDemo from './EasingExample';
 import PressableRefreshDemo from './PressableRefresh';
 import ShareExample from './ShareExample';
+import Swiper from './Swiper';
+import SwiperCopy from './SwiperDemo';
 import SwitchComponent from './SwitchComponent';
 import VibrationDemo from './VibrationExample';
 
 const App=() =>{
   return (
     <View style={{flex:1,flexDirection:'column'}}>
-     <VibrationDemo/>
+     <SwiperCopy/>
     </View>
     )
 }
---
+++ SwiperDemo.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {View,StyleSheet, Text, Button, TouchableOpacity, Platform} from 'react-native';
+import Swiper from 'react-native-deck-swiper';
+
+const SwiperCopy=props=>{
+    return(
+            <Swiper
+            useViewOverflow={Platform.os==='ios'}
+            cards={['Do','More','Of','Makes','You','Happy']}
+                renderCard={(card)=>{
+                    console.log(card)
+                    return(
+                    <View style={styles.card}>
+                        <Text style={styles.text}>{card}</Text>
+                    </View>
+                    )
+                }}
+                cardIndex={0}
+                showSecondCard={true}
+                verticalSwipe={false}
+                disableBottomSwipe={true}
+                disableTopSwipe={true}
+                infinite={true}
+                onSwipedRight={(cardIndex)=>console.log(cardIndex)}
+                onSwipedLeft={(cardIndex)=>console.log(cardIndex)}
+                stackScale={5}
+                stackSize={5}
+                
+            >
+                
+            </Swiper>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    card: {
+      flex: 1,
+      borderRadius: 4,
+      justifyContent: "center",
+      backgroundColor: "white"
+    },
+    text: {
+      textAlign: "center",
+      fontSize: 50,
+      backgroundColor: "transparent"
+    }
+  });
+
+  export default SwiperCopy;
